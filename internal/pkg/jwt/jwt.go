0defcf57bb6dcc586d280fb1ed35cbc779526f8e
Rafael Thayto <rafa.thayto@gmail.com>
2024-06-19 16:48:35 -0300

    feat(jwt): adds jwt package

---
+++ internal/pkg/jwt/jwt.go
@@ -0,0 +1,40 @@
+package jwt
+
+import (
+	"log"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// secret key being used to sign tokens
+var (
+	SecretKey = []byte("Secret")
+)
+
+func GenerateToken(username string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	/* Create a map to store our claims */
+	claims := token.Claims.(jwt.MapClaims)
+	/* Set token claims */
+	claims["username"] = username
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	tokenString, err := token.SignedString(SecretKey)
+	if err != nil {
+		log.Fatal("Error in Generating Key")
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ParseToken(tokenStr string) (string, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return SecretKey, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		username := claims["username"].(string)
+		return username, nil
+	} else {
+		return "", err
+	}
+}
